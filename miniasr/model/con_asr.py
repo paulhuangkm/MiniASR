720f2ec5789430fcbaf80fb0a67e85610d5391c3
b08902072 <paulpaul@gmail.com>
2022-02-05 12:52:15 +0800

    test conformer

--- miniasr/model/con_asr.py
+++ miniasr/model/con_asr.py
@@ -23,17 +23,28 @@ class ASR(BaseASR):
         super().__init__(tokenizer, args)
 
         self.prenet = nn.Linear(self.in_dim, args.model.encoder.dim)
-        self.sublayer = ConformerBlock(
+        self.encoder = nn.Sequential(
+            self.prenet,
+            ConformerBlock(
+                dim_head = 64,
+                heads = 2,
+                **args.model.encoder
+            ),
+            ConformerBlock(
+                dim_head = 64,
+                heads = 2,
+                **args.model.encoder
+            ),
+            ConformerBlock(
+                dim_head = 64,
+                heads = 2,
+                **args.model.encoder
+            ),
+            ConformerBlock(
                 dim_head = 64,
                 heads = 2,
                 **args.model.encoder
             )
-        self.encoder = nn.Sequential(
-            self.prenet,
-            self.sublayer,
-            self.sublayer,
-            self.sublayer,
-            self.sublayer
         )
 
         self.ctc_output_layer = nn.Linear(
